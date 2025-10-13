d57d38d93e91c7a6a437ad92b463e4b2992506dd
Rahmatjon Matkarimov <rahmatjon@192.168.1.8>
2025-10-13 22:00:26 +0500

    1

--- router/auth.routes.js
+++ router/auth.routes.js
@@ -1,15 +1,17 @@
 const { Router } = require("express");
-const { register, login, logout } = require("../controller/auth.controller");
+const { register, login, logout, forgotPassVerify, resetPassword } = require("../controller/auth.controller");
 const { verify } = require("jsonwebtoken");
 const refreshToken = require("../middleware/refreshToken");
 const authValidator = require("../validator/auth.validator");
 const authRouter = Router();
 
 
-authRouter.post("/register",authValidator,register)
-authRouter.post("/verify",verify)
-authRouter.post("/login",login)
-authRouter.post("/refreshToken",refreshToken)
-authRouter.get("/logout",logout)
+authRouter.post("/register", authValidator, register)
+authRouter.post("/verify", verify)
+authRouter.post("/login", login)
+authRouter.post("/refreshToken", refreshToken)
+authRouter.get("/logout", logout)
+authRouter.post("/forgotPasswordVerify", forgotPassVerify)
+authRouter.post("/resetPassword", resetPassword)
 
 module.exports = authRouter;
