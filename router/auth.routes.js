e305cb03c42a2bc7c1a03bcb204d52ae2e8f7491
Rahmatjon Matkarimov <rahmatjon@192.168.1.8>
2025-10-13 21:44:11 +0500

    1

--- router/auth.routes.js
+++ router/auth.routes.js
@@ -2,10 +2,11 @@ const { Router } = require("express");
 const { register, login, logout } = require("../controller/auth.controller");
 const { verify } = require("jsonwebtoken");
 const refreshToken = require("../middleware/refreshToken");
+const authValidator = require("../validator/auth.validator");
 const authRouter = Router();
 
 
-authRouter.post("/register",register)
+authRouter.post("/register",authValidator,register)
 authRouter.post("/verify",verify)
 authRouter.post("/login",login)
 authRouter.post("/refreshToken",refreshToken)
---
+++ validator/auth.validator.js
@@ -0,0 +1,17 @@
+const joi = require("joi")
+
+const authValidator = (data) => {
+    const schema = joi.object({
+        username: joi.string().min(3).max(80).required(),
+        email: joi.string().min(3).max(80).required(),
+        password: joi.string().min(8).max(80).required(),
+        role: joi.string().enum(["user", "admin", "superAdmin"]).required(),
+        otp: joi.string().required(),
+        otpTime: joi.number().integer().required(),
+        isVerified: joi.boolean().default(false),
+    })
+
+    return schema.validate(data)
+}
+
+module.exports = authValidator
